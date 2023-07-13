f7602a8a7c70e3b1f600c258637419b2d19385a8
FelAlmanzar <almanzar769@gmail.com>
2023-07-13 18:23:42 -0500

    Filtros

--- script.js
+++ script.js
@@ -74,247 +74,385 @@
 
 // alert("Gracias por su compra")
 
-let contenedor = document.getElementById("contenedor")
+// let contenedor = document.getElementById("contenedor")
 
-document.addEventListener('DOMContentLoaded', () =>{
-    lista()
-})
+// document.addEventListener('DOMContentLoaded', () =>{
+//     lista()
+// })
+
+// const lista = async () => {
+//     try{
+//     const res = await fetch('data.json')
+//     const data = await res.json()
+//     games(data)
+// } catch (error){
+//     console.log (error)
+// }
+
+// }
+
+
+// const games = data => {
+//     console.log(data)
+//     data.forEach(productoJuego => {
+//         console.log(productoJuego.nombre)
+
+//         let contenedorProd = document.createElement("div")
+
+//     contenedorProd.innerHTML = `
+//     <div>
+//             <img src="${productoJuego.imagen}" alt="producto 1">
+//             <div class="informacion">
+//                 <p>${productoJuego.nombre}</p>
+//                 <p class="Género">${productoJuego.genero}</p>
+//                 <p class="precio">$${productoJuego.precio}</p>
+//                 <button class="btnComprar" id="btnComprar">Comprar</button>
+//             </div>
+//         </div>
+
+//         `
+
+//         contenedor.append(contenedorProd)
+
+//         let juegosTodos =document.getElementById("juegosTodos")
+//         juegosTodos.addEventListener("click", filtroTodos)
+
+//         let juegosAventura =document.getElementById("juegosAventura")
+//         juegosAventura.addEventListener("click", filtroAventura)
+
+//         let juegosCarreras =document.getElementById("juegosCarreras")
+//         juegosCarreras.addEventListener("click", filtroCarreras)
+
+//         let juegosVersus =document.getElementById("juegosVersus")
+//         juegosVersus.addEventListener("click", filtroVersus)
+
+//         let juegosDeportes =document.getElementById("juegosDeportes")
+//         juegosDeportes.addEventListener("click", filtroDeportes)
+
+        
+//     })
+//     function cargarProductos(){
+//         return (games);
+//     }
+//     console.log(mostrar)
+
+//     function filtroTodos(){
+//         const resultadoTodos = data
+//         // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
+//         console.log(resultadoTodos);
+//         cargarProductos(resultadoTodos);
+//     }
+    
+//     function filtroAventura(){
+//         const resultadoAventura = data.filter(producto => producto.genero.includes("Aventura"))
+//         // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
+//         console.log(resultadoAventura);
+//         cargarProductos(resultadoAventura);
+//     }
+    
+//     function filtroCarreras(){
+//         const resultadoCarreras = data.filter(producto => producto.genero.includes("Carreras"))
+//         // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
+//         console.log(resultadoCarreras);
+//         cargarProductos(resultadoCarreras);
+//     }
+    
+//     function filtroVersus(){
+//         const resultadoVersus = data.filter(producto => producto.genero.includes("Versus"))
+//         // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
+//         console.log(resultadoVersus);
+//         cargarProductos(resultadoVersus);
+//     }
+    
+//     function filtroDeportes(){
+//         const resultadoDeportes = data.filter(producto => producto.genero.includes("Deportes"))
+//         // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
+//         console.log(resultadoDeportes);
+//         cargarProductos(resultadoDeportes);
+//     }
+    
+// }
+
+let contenedor = document.getElementById("contenedor");
+
+document.addEventListener('DOMContentLoaded', () => {
+    lista();
+});
 
 const lista = async () => {
-    try{
-    const res = await fetch('data.json')
-    const data = await res.json()
-    console.log(data)
-} catch (error){
-    console.log (error)
-}
+    try {
+        const res = await fetch('data.json');
+        const data = await res.json();
+        games(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-}
+const games = (data) => {
+    console.log(data);
+    data.forEach((productoJuego) => {
+        console.log(productoJuego.nombre);
+
+        let contenedorProd = document.createElement("div");
+
+        contenedorProd.innerHTML = `
+            <div>
+                <img src="${productoJuego.imagen}" alt="producto 1">
+                <div class="informacion">
+                    <p>${productoJuego.nombre}</p>
+                    <p class="Género">${productoJuego.genero}</p>
+                    <p class="precio">$${productoJuego.precio}</p>
+                    <button class="btnComprar" id="btnComprar">Comprar</button>
+                </div>
+            </div>
+        `;
+
+        contenedor.append(contenedorProd);
+    });
+
+    let juegosTodos = document.getElementById("juegosTodos");
+    juegosTodos.addEventListener("click", filtroTodos);
+
+    let juegosAventura = document.getElementById("juegosAventura");
+    juegosAventura.addEventListener("click", filtroAventura);
+
+    let juegosCarreras = document.getElementById("juegosCarreras");
+    juegosCarreras.addEventListener("click", filtroCarreras);
+
+    let juegosVersus = document.getElementById("juegosVersus");
+    juegosVersus.addEventListener("click", filtroVersus);
+
+    let juegosDeportes = document.getElementById("juegosDeportes");
+    juegosDeportes.addEventListener("click", filtroDeportes);
+
+    function cargarProductos(productos) {
+        contenedor.innerHTML = ""; // Limpiar el contenedor antes de cargar los productos
+
+        productos.forEach((producto) => {
+            let contenedorProd = document.createElement("div");
+
+            contenedorProd.innerHTML = `
+                <div>
+                    <img src="${producto.imagen}" alt="producto 1">
+                    <div class="informacion">
+                        <p>${producto.nombre}</p>
+                        <p class="Género">${producto.genero}</p>
+                        <p class="precio">$${producto.precio}</p>
+                        <button class="btnComprar" id="btnComprar">Comprar</button>
+                    </div>
+                </div>
+            `;
+
+            contenedor.append(contenedorProd);
+        });
+    }
+
+    function filtroTodos() {
+        cargarProductos(data);
+    }
+
+    function filtroAventura() {
+        const resultadoAventura = data.filter((producto) => producto.genero.includes("Aventura"));
+        cargarProductos(resultadoAventura);
+    }
+
+    function filtroCarreras() {
+        const resultadoCarreras = data.filter((producto) => producto.genero.includes("Carreras"));
+        cargarProductos(resultadoCarreras);
+    }
+
+    function filtroVersus() {
+        const resultadoVersus = data.filter((producto) => producto.genero.includes("Versus"));
+        cargarProductos(resultadoVersus);
+    }
+
+    function filtroDeportes() {
+        const resultadoDeportes = data.filter((producto) => producto.genero.includes("Deportes"));
+        cargarProductos(resultadoDeportes);
+    }
+};
 
 
 
 
-const productosJuegos = [
-    {
-        id: 1,
-        nombre: "Fifa 23",
-        precio: 300000,
-        genero: "Deportes",
-        stock: "50",
-        imagen: "images/Fifa-23.webp",
-    },
 
-    {
-        id: 2,
-        nombre: "Fifa 22",
-        precio: 90000,
-        genero: "Deportes",
-        stock: "50",
-        imagen: "images/Fifa-22.webp",
-    },
 
-    {
-        id: 3,
-        nombre: "Halo Infinite",
-        precio: 255000,
-        genero: "Aventura",
-        stock: "50",
-        imagen: "images/halo_infinite.jpg",
-    },
+// const productosJuegos = [
+//     {
+//         id: 1,
+//         nombre: "Fifa 23",
+//         precio: 300000,
+//         genero: "Deportes",
+//         stock: "50",
+//         imagen: "images/Fifa-23.webp",
+//     },
 
-    {
-        id: 4,
-        nombre: "e-football 23",
-        precio: 220000,
-        genero: "Deportes",
-        stock: "50",
-        imagen: "images/efootball-2023.jpg",
-    },
+//     {
+//         id: 2,
+//         nombre: "Fifa 22",
+//         precio: 90000,
+//         genero: "Deportes",
+//         stock: "50",
+//         imagen: "images/Fifa-22.webp",
+//     },
 
-    {
-        id: 5,
-        nombre: "Fornite",
-        precio: 130000,
-        genero: "Versus",
-        stock: "50",
-        imagen: "images/Fortnite.jpg",
-    },
+//     {
+//         id: 3,
+//         nombre: "Halo Infinite",
+//         precio: 255000,
+//         genero: "Aventura",
+//         stock: "50",
+//         imagen: "images/halo_infinite.jpg",
+//     },
 
-    {
-        id: 6,
-        nombre: "Assassins Creed",
-        precio: 180000,
-        genero: "Aventura",
-        stock: "50",
-        imagen: "images/Assassins.jpg",
-    },
+//     {
+//         id: 4,
+//         nombre: "e-football 23",
+//         precio: 220000,
+//         genero: "Deportes",
+//         stock: "50",
+//         imagen: "images/efootball-2023.jpg",
+//     },
 
-    {
-        id: 7,
-        nombre: "God of War",
-        precio: 280000,
-        genero: "Aventura",
-        stock: "50",
-        imagen: "images/God of war.jpeg",
-    },
+//     {
+//         id: 5,
+//         nombre: "Fornite",
+//         precio: 130000,
+//         genero: "Versus",
+//         stock: "50",
+//         imagen: "images/Fortnite.jpg",
+//     },
 
-    {
-        id: 8,
-        nombre: "Street Fighter 6",
-        precio: 320000,
-        genero: "Versus",
-        stock: "50",
-        imagen: "images/Street.jfif",
-    },
+//     {
+//         id: 6,
+//         nombre: "Assassins Creed",
+//         precio: 180000,
+//         genero: "Aventura",
+//         stock: "50",
+//         imagen: "images/Assassins.jpg",
+//     },
 
-    {
-        id: 9,
-        nombre: "Call of Duty - Warzone",
-        precio: 200000,
-        genero: "Versus",
-        stock: "50",
-        imagen: "images/COD-WARZONE.webp",
-    },
+//     {
+//         id: 7,
+//         nombre: "God of War",
+//         precio: 280000,
+//         genero: "Aventura",
+//         stock: "50",
+//         imagen: "images/God of war.jpeg",
+//     },
 
-    {
-        id: 10,
-        nombre: "Mario Kart 8 Deluxe",
-        precio: 240000,
-        genero: "Carreras",
-        stock: "50",
-        imagen: "images/mario-kart-deluxe-8.webp",
-    },
+//     {
+//         id: 8,
+//         nombre: "Street Fighter 6",
+//         precio: 320000,
+//         genero: "Versus",
+//         stock: "50",
+//         imagen: "images/Street.jfif",
+//     },
 
-    {
-        id: 11,
-        nombre: "NFS Unbound",
-        precio: 160000,
-        genero: "Carreras",
-        stock: "50",
-        imagen: "images/Need_for_Speed_Unbound.png",
-    },
+//     {
+//         id: 9,
+//         nombre: "Call of Duty - Warzone",
+//         precio: 200000,
+//         genero: "Versus",
+//         stock: "50",
+//         imagen: "images/COD-WARZONE.webp",
+//     },
 
-    {
-        id: 12,
-        nombre: "Dragon Ball FighterZ",
-        precio: 95000,
-        genero: "Aventura",
-        stock: "50",
-        imagen: "images/DBZ-Fighter.jpg",
-    },
-]
+//     {
+//         id: 10,
+//         nombre: "Mario Kart 8 Deluxe",
+//         precio: 240000,
+//         genero: "Carreras",
+//         stock: "50",
+//         imagen: "images/mario-kart-deluxe-8.webp",
+//     },
 
-console.log(productosJuegos)
+//     {
+//         id: 11,
+//         nombre: "NFS Unbound",
+//         precio: 160000,
+//         genero: "Carreras",
+//         stock: "50",
+//         imagen: "images/Need_for_Speed_Unbound.png",
+//     },
+
+//     {
+//         id: 12,
+//         nombre: "Dragon Ball FighterZ",
+//         precio: 95000,
+//         genero: "Aventura",
+//         stock: "50",
+//         imagen: "images/DBZ-Fighter.jpg",
+//     },
+// ]
+
+// console.log(productosJuegos)
 
 
 let categoria = document.getElementsByClassName("categoria")
 let botonAgregar = document.querySelectorAll(".productoAgregar")
 
 console.log(contenedor)
-function cargarProductos(productosElegidos){
+// function cargarProductos(productosElegidos){
 
     contenedor.innerHTML= "";
 
-productosElegidos.forEach((productoJuego)=>{
-    console.log(productoJuego.nombre)
+// productosElegidos.forEach((productoJuego)=>{
+//     console.log(productoJuego.nombre)
 
-    let contenedorProd = document.createElement("div")
+//     let contenedorProd = document.createElement("div")
 
-    contenedorProd.innerHTML = `
-    <div>
-            <img src="${productoJuego.imagen}" alt="producto 1">
-            <div class="informacion">
-                <p>${productoJuego.nombre}</p>
-                <p class="Género">${productoJuego.genero}</p>
-                <p class="precio">$${productoJuego.precio}</p>
-                <button class="btnComprar" id="btnComprar">Comprar</button>
-            </div>
-        </div>
+//     contenedorProd.innerHTML = `
+//     <div>
+//             <img src="${productoJuego.imagen}" alt="producto 1">
+//             <div class="informacion">
+//                 <p>${productoJuego.nombre}</p>
+//                 <p class="Género">${productoJuego.genero}</p>
+//                 <p class="precio">$${productoJuego.precio}</p>
+//                 <button class="btnComprar" id="btnComprar">Comprar</button>
+//             </div>
+//         </div>
 
-        `
+//         `
 
-        contenedor.append(contenedorProd)
+//         contenedor.append(contenedorProd)
 
 
-        let juegosTodos =document.getElementById("juegosTodos")
-        juegosTodos.addEventListener("click", filtroTodos)
 
-        let juegosAventura =document.getElementById("juegosAventura")
-        juegosAventura.addEventListener("click", filtroAventura)
-
-        let juegosCarreras =document.getElementById("juegosCarreras")
-        juegosCarreras.addEventListener("click", filtroCarreras)
-
-        let juegosVersus =document.getElementById("juegosVersus")
-        juegosVersus.addEventListener("click", filtroVersus)
-
-        let juegosDeportes =document.getElementById("juegosDeportes")
-        juegosDeportes.addEventListener("click", filtroDeportes)
         
        
-        const productosEnCarrito = [];
+//         const productosEnCarrito = [];
         
 
-        let btnComprar = document.getElementById("btnComprar");
+//         let btnComprar = document.getElementById("btnComprar");
 
         
 
-        btnComprar.addEventListener("click", agregarCarrito)
+//         btnComprar.addEventListener("click", agregarCarrito)
 
-        btnComprar.addEventListener("click",() =>{
-            productosEnCarrito.push({
-                id: productoJuego.id,
-                img: productoJuego.imagen,
-                nombre: productoJuego.nombre,
-                precio: productoJuego.precio,
-            });
-            console.log(productosEnCarrito);
-        });
+//         btnComprar.addEventListener("click",() =>{
+//             productosEnCarrito.push({
+//                 id: productoJuego.id,
+//                 img: productoJuego.imagen,
+//                 nombre: productoJuego.nombre,
+//                 precio: productoJuego.precio,
+//             });
+//             console.log(productosEnCarrito);
+//         });
 
 
-});
-}
+// });
+// }
 
-cargarProductos(productosJuegos);
-
-function filtroTodos(){
-    const resultadoTodos = productosJuegos
-    // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
-    console.log(resultadoTodos);
-    cargarProductos(resultadoTodos);
-}
-
-function filtroAventura(){
-    const resultadoAventura = productosJuegos.filter(producto => producto.genero.includes("Aventura"))
-    // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
-    console.log(resultadoAventura);
-    cargarProductos(resultadoAventura);
-}
-
-function filtroCarreras(){
-    const resultadoCarreras = productosJuegos.filter(producto => producto.genero.includes("Carreras"))
-    // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
-    console.log(resultadoCarreras);
-    cargarProductos(resultadoCarreras);
-}
-
-function filtroVersus(){
-    const resultadoVersus = productosJuegos.filter(producto => producto.genero.includes("Versus"))
-    // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
-    console.log(resultadoVersus);
-    cargarProductos(resultadoVersus);
-}
-
-function filtroDeportes(){
-    const resultadoDeportes = productosJuegos.filter(producto => producto.genero.includes("Deportes"))
-    // const resultado = productos.filter((el) => el.genero.includes("fútbol"))
-    console.log(resultadoDeportes);
-    cargarProductos(resultadoDeportes);
-}
+// cargarProductos(productosJuegos);
 
 
 
 
-function agregarCarrito(){
-    console.log("agregar carrito")
-}
+
+
+// function agregarCarrito(){
+//     console.log("agregar carrito")
+// }
